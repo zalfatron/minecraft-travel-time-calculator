bd04ce2bdf93fc703adb375d9d7d23b81c5ec17d
zalfatron <zalfatron@gmail.com>
2020-11-17 17:27:57 -0600

    Add main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# ERROR on lines 2,3,5,6: bad operand type for abs(): 'str'
+current_x_cord = str(int(abs(input("Current X coordinate: "))))
+current_z_cord = str(int(abs(input("Current Z coordinate: "))))
+
+destination_x_cord = str(int(abs(input("Destination X coordinate"))))
+destination_z_cord = str(int(abs(input("Destination Z coordinate"))))
+
+distance_x = destination_x_cord - current_x_cord
+distance_z = destination_z_cord - current_z_cord
+
+print(distance_x)
+print(distance_z)
+
+# Blocks Per Hour: 20,160
