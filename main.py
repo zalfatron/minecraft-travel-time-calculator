0591811f9dc3d6a07a579a3989c83ff8c4f07433
zalfatron <zalfatron@gmail.com>
2020-11-17 21:13:14 -0600

    Fix TypeError

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # ERROR on lines 2,3,5,6: bad operand type for abs(): 'str'
-current_x_cord = str(abs(int(input("Current X coordinate: "))))
-current_z_cord = str(abs(int(input("Current Z coordinate: "))))
+current_x_cord = abs(int(input("Current X coordinate: ")))
+current_z_cord = abs(int(input("Current Z coordinate: ")))
 
-destination_x_cord = str(abs(int(input("Destination X coordinate"))))
-destination_z_cord = str(abs(int(input("Destination Z coordinate"))))
+destination_x_cord = abs(int(input("Destination X coordinate")))
+destination_z_cord = abs(int(input("Destination Z coordinate")))
 
 distance_x = destination_x_cord - current_x_cord
 distance_z = destination_z_cord - current_z_cord
