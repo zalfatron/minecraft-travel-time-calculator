eabc9c3ab9552070130c3262811e0d9cabdb9a1e
zalfatron <zalfatron@gmail.com>
2020-11-17 22:49:43 -0600

    Rename variables.

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-current_x_cord = abs(int(input("Current X coordinate: ")))
-current_z_cord = abs(int(input("Current Z coordinate: ")))
+current_x = abs(int(input("Current X coordinate: ")))
+current_z = abs(int(input("Current Z coordinate: ")))
 
-destination_x_cord = abs(int(input("Destination X coordinate")))
-destination_z_cord = abs(int(input("Destination Z coordinate")))
+dest_x = abs(int(input("Destination X coordinate")))
+dest_z = abs(int(input("Destination Z coordinate")))
 
-distance_x = destination_x_cord - current_x_cord
-distance_z = destination_z_cord - current_z_cord
+distance_x = abs(dest_x - current_x)
+distance_z = abs(dest_z - current_z)
 
 print(distance_x)
 print(distance_z)
 
 # Blocks Per Hour: 20,160
+# distance/20,160 = 6
