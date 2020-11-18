c06c5493c8ed271449f64e84395a3410b1448fb6
zalfatron <zalfatron@gmail.com>
2020-11-18 00:14:09 -0600

    Final look-over.

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
+import math
+
 current_x = abs(int(input("Current X coordinate: ")))
 current_z = abs(int(input("Current Z coordinate: ")))
 
-dest_x = abs(int(input("Destination X coordinate")))
-dest_z = abs(int(input("Destination Z coordinate")))
-
+dest_x = abs(int(input("Destination X coordinate: ")))
+dest_z = abs(int(input("Destination Z coordinate: ")))
+#if():
 dist_x = abs(dest_x - current_x)
+
+#if():
 dist_z = abs(dest_z - current_z)
 
-print(dist_x)
-print(dist_z)
+dist_x_sqd = dist_x ** 2 
+dist_z_sqd = dist_z ** 2
 
+dist_sqd = dist_x_sqd + dist_z_sqd
+
+print(dist_sqd)
+
+distance = math.sqrt(dist_sqd)
+
+print(distance)
+
+output = distance / 20160
+print(output)
 # Blocks Per Hour: 20,160
 # distance/20,160 = 6
