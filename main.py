9790254767d55c203b94dd01af68c7d6f703b2c5
zalfatron <zalfatron@gmail.com>
2020-11-17 23:05:36 -0600

    Rename variables.

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ current_z = abs(int(input("Current Z coordinate: ")))
 dest_x = abs(int(input("Destination X coordinate")))
 dest_z = abs(int(input("Destination Z coordinate")))
 
-distance_x = abs(dest_x - current_x)
-distance_z = abs(dest_z - current_z)
+dist_x = abs(dest_x - current_x)
+dist_z = abs(dest_z - current_z)
 
-print(distance_x)
-print(distance_z)
+print(dist_x)
+print(dist_z)
 
 # Blocks Per Hour: 20,160
 # distance/20,160 = 6
