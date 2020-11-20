0e4a15ba39f00d8f3f88e9e13928aa5d9369b13c
zalfatron <zalfatron@gmail.com>
2020-11-20 09:38:11 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -8,25 +8,13 @@ current_z = abs(int(input("Current Z coordinate: ")))
 dest_x = abs(int(input("Destination X coordinate: ")))
 dest_z = abs(int(input("Destination Z coordinate: ")))
 
-# Find the length of side A and B
-dist_x = abs(dest_x - current_x)
-dist_z = abs(dest_z - current_z)
-
-# Square sides A and B
-dist_x_sqd = dist_x ** 2 
-dist_z_sqd = dist_z ** 2
-
-# Add the squared side A and side B
-dist_sqd = dist_x_sqd + dist_z_sqd
-
-# Finds the distance between the two sets of coordinates given
-distance = math.sqrt(dist_sqd)
+distance = math.sqrt((dest_x - current_x) ** 2 + (dest_z - current_z))
 
 # Calculats how long it will take to travel the specified distance
 output = str(distance / 20160)
 
 # Prints the estimated travel time
-print("You're journey will take " + output + "hours")
+print("You're journey will take " + output + " hours.")
 
 # Blocks Per Hour: 20,160
 # distance/20,160 = 6
